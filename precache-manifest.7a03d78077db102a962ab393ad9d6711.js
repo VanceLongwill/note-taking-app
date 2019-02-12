6f6debc8eabef9029f0f3339884163eae4aba91d
Vance Longwill <vancelongwill@gmail.com>
2019-02-12 12:26:35 +0300

    Updates

---
+++ precache-manifest.7a03d78077db102a962ab393ad9d6711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d162c2ae21c56e21d9a1",
+    "url": "/note-taking-app/static/js/runtime~main.d162c2ae.js"
+  },
+  {
+    "revision": "cab66376a90ec69e10d0",
+    "url": "/note-taking-app/static/js/main.cab66376.chunk.js"
+  },
+  {
+    "revision": "8029367f40bfcc62ad07",
+    "url": "/note-taking-app/static/js/2.8029367f.chunk.js"
+  },
+  {
+    "revision": "cab66376a90ec69e10d0",
+    "url": "/note-taking-app/static/css/main.23399065.chunk.css"
+  },
+  {
+    "revision": "fa8425ca9afa1715c572af99ad012526",
+    "url": "/note-taking-app/index.html"
+  }
+];
